01d7c6409dce353ccaf978243b84583661674650
6ciias <129872241+6ciias@users.noreply.github.com>
2023-04-25 11:18:03 -0400

    added comments to challenge

--- in-class-coding-challenge.js
+++ in-class-coding-challenge.js
@@ -1,7 +1,7 @@
 // create a function that filters out negative numbers
-var numbers = [-2,-4,10];
-console.log(numbers);
+var numbers = [-2,-4,10]; // Pick the numbers that you want to check here
+console.log(numbers); // Original numbers will output here -2, -4, 10
 numbers = numbers.filter(function(x){
 return x > -1
 });
-console.log(numbers);
+console.log(numbers); // The negative numbers will be filtered out here.
